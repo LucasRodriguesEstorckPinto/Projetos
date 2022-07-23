14991e4fe05c0ae1fdb94cd66a0cf5c4bdee9a0f
LucasRodriguesEstorckPinto <109537651+LucasRodriguesEstorckPinto@users.noreply.github.com>
2022-07-22 23:00:09 -0300

    Update codigo.py

--- projeto7/codigo.py
+++ projeto7/codigo.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 def titulo(texto):
     tamanho = len(texto) + 15
     linhas = '~' * tamanho
@@ -24,8 +26,47 @@ def atribui(nome, notas, arquivo):
 
     arquivo.write('\n')
 
+def condicao(valor):
+    while True:
+        if valor[0] in 'Ss':
+            return 1
+        elif valor[0] in 'Nn':
+            return 0 
+        else:
+            print('\033[31mPor favor, digite o que foi pedido\033[m')
+            continue
+
+def opcoes():
+    sleep(1)
+    print('\nO QUE DESEJA FAZER?\n')
+    sleep(1)
+    print('[1] CADASTRAR ALUNO E SUAS NOTAS\n[2] VER ALUNOS CADASTRADOS\n[3] SAIR DO PROGRAMA')
+    print('~'*33)
+
+
+def inteiro(numero):
+    while True:
+        try:
+            valor = int(input(numero))
+        except:
+            print('\033[31mPor favor, digite um número\033[m')
+        else:
+            return int(valor)
+            
+
 
 verifica()
-while True:
-    titulo('CADASTRO DE NOTAS')
-    break
+titulo('CADASTRO DE ALUNOS')
+while True: 
+    opcoes()
+    escolha = inteiro('Digite aqui sua escolha: ')
+    if escolha == 1:
+        print(' AQUI VAI CHAMAR FUNÇÕES PARA CADASTRO')    
+    if escolha == 2:
+        print('AQUI VAI CHAMAR A FUNÇÃO PARA MOSTRAR O ARQUIVO DE ALUNOS')
+    if escolha == 3:
+        break
+    elif  escolha >3 or escolha <1:
+        print('\033[31mPOR FAVOR DIGITE UMA DAS OPÇÕES\033[m')
+    
+    
