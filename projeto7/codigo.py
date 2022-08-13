799b863c4b6e1d238c4465d4068d1031435d2c68
LucasRodriguesEstorckPinto <109537651+LucasRodriguesEstorckPinto@users.noreply.github.com>
2022-08-12 22:55:42 -0300

    feito

--- projeto7/codigo.py
+++ projeto7/codigo.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 def titulo(texto):
-    tamanho = len(texto) + 15
+    tamanho = len(texto) + 20
     linhas = '~' * tamanho
     print(linhas)
     print(f'\033[32m{texto.center(tamanho)}\033[m')
@@ -14,17 +14,21 @@ def verifica():
     except:
         print('Arquivo (notas.txt) não foi encontrado, portanto, será criado')
         open('notas.txt', 'a')
+        arquivo = open('notas.txt','a')
+        arquivo.write(f'{"nome":<10}{"Nota 1":<15}{"Nota 2":<15}{"Nota 3":<15}{"Media":<15}\n{"_"*80}\n')
+        arquivo.close()
     else:
         print('Arquivo (notas.txt) foi encontrado, nada feito')
 
 
-def atribui(nome, notas, arquivo):
+def atribui(nome, notas):
+    arquivo = open('notas.txt','a')
     arquivo.write(f'{nome :<10}')
     for c in range(0, 4):
-        arquivo.write(f'{notas[c]:<5}')
-        arquivo.write(f'{notas[c]:<5}')
+        arquivo.write(f'{notas[c]:<15}')
 
     arquivo.write('\n')
+    arquivo.close()
 
 def condicao(valor):
     while True:
@@ -41,7 +45,7 @@ def opcoes():
     print('\nO QUE DESEJA FAZER?\n')
     sleep(1)
     print('[1] CADASTRAR ALUNO E SUAS NOTAS\n[2] VER ALUNOS CADASTRADOS\n[3] SAIR DO PROGRAMA')
-    print('~'*33)
+    print('~'*38)
 
 
 def inteiro(numero):
@@ -72,17 +76,19 @@ while True:
     if escolha == 1:
         titulo('CADASTRANDO ALUNO')
         lista = []
+        lista.clear()
         name = str(input('Nome do aluno: ')).lower().title().strip()
         nota1 = flutuante('Digite a primeira nota: ')
-        lista.append(nota1)
+        lista.append(round(nota1))
         nota2 = flutuante('digite a segunda nota:')
-        lista.append(nota2)
+        lista.append(round(nota2))
         nota3 = flutuante('digite a terceira nota:')
-        lista.append(nota3)
+        print('~'*37)
+        lista.append(round(nota3))
         media = (nota1 + nota2 + nota3) / 3
-        lista.append(media)
-        print(lista)
-        atribui('Lucas', lista, 'notas.txt')
+        lista.append(round(media))
+        atribui(name, lista)
+        lista.clear()
 
     if escolha == 2:
         arqui = open('notas.txt', encoding='utf-8')
