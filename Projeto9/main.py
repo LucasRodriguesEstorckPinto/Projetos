91cef361adb02de335b5bf717e998dec17252928
LucasRodriguesEstorckPinto <109537651+LucasRodriguesEstorckPinto@users.noreply.github.com>
2024-01-15 15:01:53 -0300

    Update main.py
    finish

--- Projeto9/main.py
+++ Projeto9/main.py
@@ -26,15 +26,46 @@ def catch_number_string(road):
                 if len(part) == 2:
                     name , number = part
                     names.append(name)
-                    numbers.append(number)
+                    numbers.append(int(number))
                 else:
                     print("Error in lists")
     
     return names , numbers
 
-def summemory_average(memory):
+def summemory(memory):
     total = 0
-    average = 0
-    for sum in memory:
-        total +=sum
-    
+    for sume in memory:
+        total +=sume
+    return total
+
+def average_memory_usage(totalmemory , memory , position):
+    #calculo a media de uso de acordo com o total de memoria
+    return (memory[position] * 100)/totalmemory
+
+
+
+def byte_to_megabyte(byte):
+    megabyte = byte / (1024 **2)
+    return megabyte
+
+
+#PROGRAMA PRINCIPAL
+
+try:
+    file = open("relatorio.txt",'w')
+except:
+    print("Ocorreu um erro ao abrir/criar o relatorio")
+else:
+    file.write(f'{"ACME Inc.":<30}{"Uso do espaço em disco pelos usuários"}\n{"--"*38}\n{"Nr.":<10}{"Usuário":<20}{"Espaço utilizado":<27}{"% do uso"}\n')
+    lists = catch_number_string(road)
+    suma = summemory(lists[1])
+
+    for c in range(0,len(lists[0])):
+        average = average_memory_usage(suma , lists[1] , c)
+        megabyted = byte_to_megabyte(lists[1][c])
+        file.write(f'{c+1:<10}{lists[0][c]:<20}{f"{megabyted:.2f} MB":<27}{f"{average:.2f} %"}\n')
+        average = 0
+        megabyted = 0
+    file.write(f"Espaço total ocupado: {byte_to_megabyte(suma):.2f} MB\nEspaço médio ocupado: {byte_to_megabyte(suma)/len(lists[1]):.2f} MB")
+    file.close()
+    print("RELATORIO.TXT CREATED")
